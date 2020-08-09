f55f93d30058cc369b29bde31e0ad608e352e4d7
Cristian David Escobar Castillo <cristianescobarcdec@gmail.com>
2020-08-09 16:39:35 -0500

    Carga archivos

--- src/keys/carga-archivos-llaves.ts
+++ src/keys/carga-archivos-llaves.ts
@@ -2,8 +2,12 @@ export namespace UploadFilesKeys {
   export const PUBLICATION_FILE_PATH = '../../uploads/publication/';
   export const ADVERTISING_IMAGE_PATH = '../../uploads/advertising/';
   export const MUSICO_PHOTO_PATH = '../../uploads/perfil-musico/';
+  export const AFICIONADO_PHOTO_PATH = '../../uploads/aficionado/';
+  export const BANDA_PHOTO_PATH = '../../uploads/banda/';
   export const PUBLICATION_FILE_FIELDNAME = 'file';
   export const MUSICO_PHOTO_FIELDNAME = 'file';
+  export const AFICIONADO_PHOTO_FIELDNAME = 'file';
+  export const BANDA_PHOTO_FIELDNAME = 'file';
   export const ADVERTISING_IMAGE_FIELDNAME = 'file';
   export const MAX_FILE_SIZE = 5 * (1024 * 1024);
   export const IMAGE_ACCEPTED_EXT: string[] = ['.PNG', '.JPG', '.JPEG', '.MP4', '.MP3'];
